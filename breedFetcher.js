56d44e2a3f480c21aead3ef1e332972ebe79c1cd
Cynthia <cynthia-lam@outlook.com>
2024-02-23 14:57:26 -0500

    Add other logs into else if block so doesn't break when error

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,14 +4,12 @@ const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
 const search = process.argv.slice(2);
 
 request(URL + search, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  
-  const data = JSON.parse(body);
-
-  if (body === '[]') {
+  if (error) {
+    console.log('error:', error);
+  } else if (body === '[]') {
     console.log('Breed name not found');
   } else {
+    const data = JSON.parse(body);
     console.log(data[0].description);
-  }
+  }  
 });
