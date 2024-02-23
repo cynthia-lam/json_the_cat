a2366bb15934c561442bea82156e987e9e17e327
Cynthia <cynthia-lam@outlook.com>
2024-02-23 16:08:52 -0500

    Made breedFetcher async, add index, M&C tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require('request');
 
 const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const search = process.argv.slice(2);
 
-request(URL + search, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else if (body === '[]') {
-    console.log('Breed name not found');
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }  
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(URL + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else if (body === '[]') {
+      callback('Breed name not found', null);
+    } else {
+      const data = JSON.parse(body);
+      const breedDescription = data[0].description;
+      callback(null, breedDescription);
+    }  
+  });
+};
+
+module.exports = {fetchBreedDescription};
